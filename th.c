d0e06d71f29426c8365e8088e6b2582a00c4f6e5
Gabriela Correia Holanda Freitas <gchf@cin.ufpe.br>
2017-10-26 11:18:32 -0300

    bug horrível

--- th.c
+++ th.c
@@ -13,75 +13,82 @@ int buffer = 0;
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t fill = PTHREAD_COND_INITIALIZER;
 
-int *ip_requests; // vetor que mostra a quantidade de requisições por ip
+int ip_requests[4]; // vetor que mostra a quantidade de requisições por ip
 
 void *consumer(void *threadid);
 void *producer(void *threadid);
 
-int *blocked_threads;
+int blocked_threads[4];
 
 void put();
 
 int main() {
 
     int counter = 0;
-    int contador = 400;
     float server_victories = 0;
     float hackers_victories = 0;
     float bugs = 0;
-    pthread_t *threads;
-    pthread_t consumer_threads[2];
-    int numbers[4] = {1,2,3,4};
     int i;
     int error;
-    int **ids;
 
-    ip_requests = (int *) calloc(numberOfThreads, sizeof(int)); // lembrar q o id das threads está com +1
-    blocked_threads = (int *) calloc(numberOfThreads, sizeof(int)); // zero = não bloqueada
-    threads = (pthread_t*) malloc(numberOfThreads*sizeof(pthread_t));
-    ids = (int **) malloc((numberOfThreads)*sizeof(int*));
-
-    for(i=0; i<numberOfThreads; i++)
+    for(numberOfThreads = 1; numberOfThreads <= 2; numberOfThreads++)
     {
-        ids[i] = (int*) malloc(sizeof(int));
-        *ids[i] = i + 1; //--------------------------------------------------^
-        error = pthread_create(&threads[i], NULL, producer, (void *) ids[i]);
+        for(counter = 0; counter < 100; counter++)
+        {
+            pthread_t threads[4]; // 4 = máximo de threads
+            int *ids[4]; // 4 = máximo de threads
+            pthread_t consumer_threads[2]; // 2 = número de servers
 
-        if(error)
-            exit(1);
+            for(i=0; i<numberOfThreads; i++)
+            {
+                ids[i] = (int*) malloc(sizeof(int));
+                *ids[i] = i; 
+                error = pthread_create(&threads[i], NULL, producer, (void *) ids[i]);
+
+                if(error)
+                    exit(1);
+            }
+
+            for(i=0; i<2; i++)
+            {
+                error = pthread_create(&consumer_threads[i], NULL, consumer, NULL);                
+                if(error)
+                    exit(1);
+            }
+
+            for(i=0; i<numberOfThreads; i++)
+                pthread_join(threads[i], NULL);
+            pthread_join(consumer_threads[0], NULL);
+            pthread_join(consumer_threads[1], NULL);
+
+            if(winner == 1)
+            {
+                hackers_victories++;
+            }
+            else if(winner == 2)
+            {
+                server_victories++;
+            }
+            else
+            {
+                bugs++;
+            }
+
+            //zerando os vetores de requests e threads bloqueadas
+            for(i=0; i<numberOfThreads; i++)
+            {
+                ip_requests[i] = 0;
+                blocked_threads[i] = 0;
+            }
+
+            winner = 0;
+            numberOfBlockedThreads = 0;
+            buffer = 0;
+        }
     }
 
-    for(i=0; i<2; i++)
-    {
-        error = pthread_create(&consumer_threads[i], NULL, consumer, NULL);
 
-        if(error)
-            exit(1);
-    }
-
-    for(i=0; i<numberOfThreads; i++)
-        pthread_join(threads[i], NULL);
-    pthread_join(consumer_threads[0], NULL);
-    pthread_join(consumer_threads[1], NULL);    
-
-    if(winner == 1)
-    {
-        hackers_victories++;
-        printf("HACKERS VENCERAM\n");
-    }
-    else if(winner == 2)
-    {
-        server_victories++;
-        printf("SERVIDOR VENCEU\n");
-    }
-    else
-    {
-        bugs++;
-        printf("deu bugs\n");
-    }
-
-
-    //printf("%f %f %f\n", bugs, server_victories, hackers_victories);
+    printf("%f %f %f\n", bugs, server_victories, hackers_victories);
     
     pthread_exit(NULL);
     return 0;
@@ -98,7 +105,7 @@ void put(long int thread_id)
     else
     {
         buffer++; // soma o buffer
-        ip_requests[thread_id - 1]++; // +1 request para o ip
+        ip_requests[thread_id]++; // +1 request para o ip
     }
 
     pthread_mutex_unlock(&mutex);
@@ -108,30 +115,30 @@ void get()
 {
     pthread_mutex_lock(&mutex);
 
-        if(buffer == 0) // não subtrai do buffer
+    if(buffer == 0) // não subtrai do buffer
+    {
+        int i;
+        for(i=0; i<numberOfThreads; i++)
         {
-            int i;
-            for(i=0; i<numberOfThreads; i++)
+            if(ip_requests[i] >= 10) 
             {
-                if(ip_requests[i] >= 10) 
+                if(blocked_threads[i] == 0)
                 {
-                    if(blocked_threads[i] == 0)
-                    {
-                        blocked_threads[i] = 1;
-                        numberOfBlockedThreads++;
-                    }
+                    blocked_threads[i] = 1;
+                    numberOfBlockedThreads++;
                 }
             }
+        }
 
-            if(numberOfBlockedThreads == numberOfThreads) // se buffer = 0 e todas threads produtoras estão bloqueadas
-            {
-                winner = 2; //servidor vence
-            }
-        }
-        else
+        if(numberOfBlockedThreads == numberOfThreads) // se buffer = 0 e todas threads produtoras estão bloqueadas
         {
-            buffer--;
+            winner = 2; //servidor vence
         }
+    }
+    else
+    {
+        buffer--;
+    }
 
     pthread_mutex_unlock(&mutex);
 }
@@ -150,7 +157,7 @@ void *consumer(void *threadid)
 void *producer(void *threadid)
 {
     long int thread_id = *( (long *) threadid );
-    while(winner == 0 && blocked_threads[thread_id - 1] == 0) // enquanto não tem vencedor, e a thread atual não está bloqueada
+    while(winner == 0 && blocked_threads[thread_id] == 0) // enquanto não tem vencedor, e a thread atual não está bloqueada
     {
         put(thread_id);
     }
